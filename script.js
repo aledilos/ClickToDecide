a00746c02ac7fb522e2229eaa3624a3b9da6f82f
aledilos <dilorenzoa31@gmail.com>
2025-02-16 20:50:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('decisionButton').addEventListener('click', function() {
+    const choices = ["sì", "no", "è un'idea di merda", "secondo me ci sta", "è una cazzata", "senti a me lascia stare", "ma si la vita è una", "ci puoi prova secondo me", ];
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    // Mostra il risultato con animazione
+    const resultElement = document.getElementById('decisionResult');
+    resultElement.innerText = randomChoice;
+    
+    // Aggiungi la classe per mostrare il risultato
+    resultElement.classList.add('show');
+
+    // Rimuovi la classe dopo un po' per nasconderlo
+    setTimeout(() => {
+        resultElement.classList.remove('show');
+    }, 6000);
+});
