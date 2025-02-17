93a8a205bd5b21a46d6a51ce3f44d8540c56c20f
Ale <ale@mac-di-ale.local>
2025-02-17 16:23:47 +0100

    Salvataggio delle modifiche locali

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ document.getElementById('decisionButton').addEventListener('click', function() {
         resultElement.classList.remove('show');
     }, 6000);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker registrato con successo:', registration);
+            })
+            .catch((error) => {
+                console.log('Registrazione Service Worker fallita:', error);
+            });
+    });
+}
